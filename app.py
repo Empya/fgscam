3254d09439e33d2096e42dc346d32a850954df71
Empya <143369038+Empya@users.noreply.github.com>
2023-12-24 00:26:29 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, session, url_for, request
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "iDi0tTest"
+
+@app.route("/")
+
+def home():
+    return render_template("home.html")
+    
+    
+@app.route("/get_funds/register/<int:amt>")
+
+def show_form(amt):
+    session["amt"] = amt
+    return render_template("signup.html")
+    
+@app.route("/get_funds/readytransfer", methods=["POST","GET"])
+
+def ready(): 
+
+    if request. method == "POST":
+        name = request.form["name"]
+        session["name"] = name
+        
+        return render_template("result.html")
+        
+    else:
+        pass
+    
+    
